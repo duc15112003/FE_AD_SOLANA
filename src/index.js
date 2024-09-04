fae3c1337aad8a39e956f9644c6274a897678ac2
thesonofwind99 <dunhan999@gmail.com>
2024-09-05 00:56:24 +0700

    UI Customer and Staff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'core-js'
-
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App'
 import store from './store'
 
