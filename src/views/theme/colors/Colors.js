acc6ea64a7792bfeea12ef961ff5f13859652153
duc15112003 <phamminhduc15112003dilinh@gmail.com>
2024-09-19 21:15:04 +0700

    fill data staff, customer to table

--- src/views/theme/colors/Colors.js
+++ src/views/theme/colors/Colors.js
@@ -1,111 +1,32 @@
-import React, { useEffect, useState, createRef } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import { CRow, CCol, CCard, CCardHeader, CCardBody } from '@coreui/react'
-import { rgbToHex } from '@coreui/utils'
-import { DocsLink } from 'src/components'
+import React, { useEffect, useState, createRef } from 'react';
+import axios from 'axios';
+const Colors = () => {
+  const [staff, setStaff] = useState([]);
 
-const ThemeView = () => {
-  const [color, setColor] = useState('rgb(255, 255, 255)')
-  const ref = createRef()
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/admin/all');
+      setStaff(response.data.result.content);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    const el = ref.current.parentNode.firstChild
-    const varColor = window.getComputedStyle(el).getPropertyValue('background-color')
-    setColor(varColor)
-  }, [ref])
+    fetchData();
+  }, []);
 
-  return (
-    <table className="table w-100" ref={ref}>
-      <tbody>
-        <tr>
-          <td className="text-body-secondary">HEX:</td>
-          <td className="font-weight-bold">{rgbToHex(color)}</td>
-        </tr>
-        <tr>
-          <td className="text-body-secondary">RGB:</td>
-          <td className="font-weight-bold">{color}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const ThemeColor = ({ className, children }) => {
-  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
-  return (
-    <CCol xs={12} sm={6} md={4} xl={2} className="mb-4">
-      <div className={classes} style={{ paddingTop: '75%' }}></div>
-      {children}
-      <ThemeView />
-    </CCol>
-  )
-}
-
-ThemeColor.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-}
-
-const Colors = () => {
   return (
     <>
-      <CCard className="mb-4">
-        <CCardHeader>
-          Thông tin chi tiết
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
-        </CCardHeader>
-        <CCardBody>
-          <CRow>
-            <div className={'col-3 mb-3 d-flex justify-content-center'}>
-              <div
-                style={{
-                  border: 'solid 1px gray',
-                  height: '198px',
-                  width: '198px',
-                  borderRadius: '99px',
-                }}
-              ></div>
-              <input type="file" className={'d-none'} />
-            </div>
-            <div className={'col-9 mb-3'}>
-              <input type="text" placeholder={'Id'} className={'form-control mb-3'} />
-              <input type="text" placeholder={'First name'} className={'form-control mb-3'} />
-              <input type="text" placeholder={'Last name'} className={'form-control mb-3'} />
-              <input type="text" placeholder={'Phone number'} className={'form-control'} />
-            </div>
-            <div className={'col-6 mb-3'}>
-              <input type="date" placeholder={'Birthday'} className={'form-control'} />
-            </div>
-            <div className={'col-6 mb-3'}>
-              <input type="text" placeholder={'Public key'} className={'form-control'} />
-            </div>
-            <div className={'col-12 mb-3'}>
-              <textarea
-                placeholder={'Address'}
-                className={'form-control'}
-                name=""
-                id=""
-                cols="30"
-                rows="6"
-              ></textarea>
-            </div>
-            <div className={'col-12 mb-3'}>
-              <button className={'btn btn-success me-2 text-light'}>Add</button>
-              <button className={'btn btn-info me-2 text-light'}>Update</button>
-              <button className={'btn btn-warning me-2'}>Delete</button>
-              <button className={'btn btn-light'}>Reset</button>
-            </div>
-          </CRow>
-        </CCardBody>
-      </CCard>
-      <CCard className="mb-4">
-        <CCardHeader>
+      <div className="card mb-4">
+        <div className="card-header">
           Danh sách nhân viên
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
-        </CCardHeader>
-        <CCardBody>
-          <CRow>
+          <a href="https://coreui.io/docs/utilities/colors/" className="ms-2">
+            DocsLink
+          </a>
+        </div>
+        <div className="card-body">
+          <div className="row">
             <div className="input-group mb-3">
               <input
                 type="text"
@@ -117,42 +38,44 @@ const Colors = () => {
                 Search
               </button>
             </div>
-            <table className={'table'} style={{ borderCollapse: 'unset' }}>
+            <table className="table" style={{ borderCollapse: 'unset' }}>
               <thead>
-                <tr>
-                  <th scope="col">Id</th>
-                  <th scope="col">First name</th>
-                  <th scope="col">Last name</th>
-                  <th scope="col">Phone number</th>
-                  <th scope="col">Birthday</th>
-                  <th scope="col">Public key</th>
-                  <th scope="col">Control</th>
-                </tr>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+                <th scope="col">Phone number</th>
+                <th scope="col">Birthday</th>
+                <th scope="col">Public key</th>
+                <th scope="col">Control</th>
+              </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                  <td>hehe</td>
-                  <td>hehe</td>
+              {staff.map((member, index) => (
+                <tr key={index}>
+                  <th scope="row">{member.id}</th>
+                  <td>{member.firstname}</td>
+                  <td>{member.lastname}</td>
+                  <td>{member.phoneNumber}</td>
+                  <td>{member.birthday}</td>
+                  <td>{member.publicKey}</td>
                   <td>
-                    <button className={'btn btn-primary me-2'}>
+                    <button className="btn btn-primary me-2">
                       <i className="bi bi-pencil-square"></i>
                     </button>
-                    <button className={'btn btn-warning'}>
+                    <button className="btn btn-warning">
                       <i className="bi bi-x-square"></i>
                     </button>
                   </td>
                 </tr>
+              ))}
               </tbody>
             </table>
-          </CRow>
-        </CCardBody>
-      </CCard>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Colors
+export default Colors;
